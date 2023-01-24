e72f897f1dc623a83296332eb4cb542ce532ef4a
CALEB <owatahcaleb@gmail.com>
2023-01-24 03:23:04 +0100

    Update webpack.config.js

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -2,37 +2,33 @@ const path = require("path");
 
 module.exports = {
   mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
   },
-  performance: {
-		maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-	},
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-        use: [
-          'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							bypassOnDebug: true,
-							disable: true,
-						},
-					},
-				],
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ['file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+                bypassOnDebug: true,
+                disable: true,
+            },
+          },
+        ],
       },
     ]
   }
-}
+};
