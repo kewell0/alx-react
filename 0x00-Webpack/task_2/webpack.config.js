92b278427b7c7e382917c0237dbe648b5d0f4bb7
CALEB <owatahcaleb@gmail.com>
2023-01-24 03:14:12 +0100

    task_2 edit

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,18 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
-    entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-};
-const path = require("path");
-
-module.exports = {
   mode: 'production',
   entry: {
     main: path.resolve(__dirname, './js/dashboard_main.js'),
@@ -47,4 +35,4 @@ module.exports = {
       },
     ]
   }
-};
+}
