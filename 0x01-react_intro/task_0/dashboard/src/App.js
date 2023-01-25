7b1f3ecef95c240101343cd947882d44ad1cb867
CALEB <owatahcaleb@gmail.com>
2023-01-25 23:19:44 +0100

    task_0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,19 @@
+import logo from './Holberton Logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
